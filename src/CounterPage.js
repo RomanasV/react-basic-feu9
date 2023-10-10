31c7d096cc5895524dc3d90e3e6c34104ba60b84
RomanasV <romanasvenckus@gmail.com>
2023-10-10 15:19:17 +0300

    counter task firs part

--- src/CounterPage.js
+++ src/CounterPage.js
@@ -4,19 +4,18 @@ import Container from './Components/Container/Container'
 const CounterPage = () => {
   const [num, setNum] = useState(5);
 
-  const minus1Handler = () => {
-    setNum(num - 1)
-  }
-
-  const plus1Handler = () => {
-    setNum(num + 1)
-  }
+  const minus2Handler = () => setNum(num - 2)
+  const minus1Handler = () => setNum(num - 1)
+  const plus1Handler = () => setNum(num + 1)
+  const plus2Handler = () => setNum(num + 2)
 
   return (
     <Container>
       <h3>{num}</h3>
-      <button onClick={minus1Handler}>-1</button>
-      <button onClick={plus1Handler}>+1</button>
+      <button onClick={minus2Handler} disabled={num <= 2}>-2</button>
+      <button onClick={minus1Handler} disabled={num <= 1}>-1</button>
+      <button onClick={plus1Handler} disabled={num > 9}>+1</button>
+      <button onClick={plus2Handler} disabled={num > 8}>+2</button>
     </Container>
   )
 }
