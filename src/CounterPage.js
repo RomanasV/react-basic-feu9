aa6fa4d863fa3f0e51df386e4920aa5feeb47d82
RomanasV <romanasvenckus@gmail.com>
2023-10-11 09:53:03 +0300

    counter task and cities task

--- src/CounterPage.js
+++ src/CounterPage.js
@@ -4,39 +4,56 @@ import Container from './Components/Container/Container'
 const CounterPage = () => {
   const initialNum = 5
   const [num, setNum] = useState(initialNum)
+  const [grades, setGrades] = useState([10, 5, 1, 6, 7])
 
-  const minus5Handler = () => setNum(prevNum => prevNum - 5)
-  const minus2Handler = () => setNum(prevNum => prevNum - 2)
-  
-  const minus1Handler = () => {
-    // setNum((prevNum) => {
-    //   const updatedNum = prevNum - 1
-    //   return updatedNum
-    // })
-
-    setNum(prevNum => prevNum - 1)
+  const counterButtonsHandler = numDifference => setNum(prevNum => prevNum + numDifference)
+  const resetHandler = () => setNum(initialNum)
+  const inputHandler = event => {
+    // setNum(event.target.valueAsNumber)
+    setNum(Number(event.target.value))
   }
 
-  const plus1Handler = () => setNum(prevNum => prevNum + 1)
-  const plus2Handler = () => setNum(prevNum => prevNum + 2)
-  const plus5Handler = () => setNum(prevNum => prevNum + 5)
-  
-  const resetHandler = () => setNum(initialNum)
+  const addGradeHandler = () => {
+    setGrades(prevState => {
+      const newState = [...prevState]
+      newState.unshift(num)
+      return newState
+    })
+  }
 
   const color = num < 5 ? 'red' : 'green'
 
   return (
     <Container>
-      {/* <h3 style={{ color: num < 5 ? 'red' : 'green' }}>{num}</h3> */}
-      {/* <h3 style={{ color: color }}>{num}</h3> */}
+      <input
+        type='number'
+        min={1}
+        max={10}
+        value={num}
+        onChange={inputHandler}
+      />
+
       <h3 style={{ color }}>{num}</h3>
-      <button onClick={minus5Handler} disabled={num <= 5}>-5</button>
-      <button onClick={minus2Handler} disabled={num <= 2}>-2</button>
-      <button onClick={minus1Handler} disabled={num <= 1}>-1</button>
+      <button onClick={() => counterButtonsHandler(-5)} disabled={num <= 5}>-5</button>
+      <button onClick={() => counterButtonsHandler(-2)} disabled={num <= 2}>-2</button>
+      <button onClick={() => counterButtonsHandler(-1)} disabled={num <= 1}>-1</button>
       <button onClick={resetHandler}>Reset</button>
-      <button onClick={plus1Handler} disabled={num > 9}>+1</button>
-      <button onClick={plus2Handler} disabled={num > 8}>+2</button>
-      <button onClick={plus5Handler} disabled={num > 5}>+5</button>
+      <button onClick={() => counterButtonsHandler(1)} disabled={num > 9}>+1</button>
+      <button onClick={() => counterButtonsHandler(2)} disabled={num > 8}>+2</button>
+      <button onClick={() => counterButtonsHandler(5)} disabled={num > 5}>+5</button>
+      <button onClick={addGradeHandler}>Add Grade</button>
+
+      <div>
+        <h4>Grades:</h4>
+        <ul>
+          {grades.map((grade, index) => (
+            <li key={index}>
+              {grade}
+              <button>X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </Container>
   )
 }
