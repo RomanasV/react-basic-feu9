f110fb1da806c699f0184a708ff172991c069bc7
RomanasV <romanasvenckus@gmail.com>
2023-10-13 13:58:07 +0300

    react router dom added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-// import ContactUsPage from './ContactUsPage'
-// import NewsPage from './NewsPage'
-// import CounterPage from './CounterPage'
-// import CitiesPage from './CitiesPage'
+import ContactUsPage from './ContactUsPage'
+import NewsPage from './NewsPage'
+import CounterPage from './CounterPage'
+import CitiesPage from './CitiesPage'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import CarsPage from './CarsPage'
+import PageHeader from './Components/PageHeader/PageHeader'
 
 function App() {
   return (
     <div>
-      {/* <NewsPage /> */}
-      {/* <ContactUsPage /> */}
-      {/* <CounterPage /> */}
-      {/* <CitiesPage /> */}
-      <CarsPage />
+      <PageHeader />
+
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>} />
+        <Route path='/cities' element={<CitiesPage />} />
+        <Route path='/counter' element={<CounterPage />} />
+        <Route path='/cars' element={<CarsPage />} />
+        <Route path='/code-academy/news' element={<NewsPage />} />
+        <Route path='/code-academy/contacts' element={<ContactUsPage />} />
+
+        <Route path='*' element={<h1>404: Page not found</h1>} />
+      </Routes>
     </div>
   )
 }
---
+++ src/Components/PageHeader/PageHeader.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Container from '../Container/Container'
+import { NavLink } from 'react-router-dom'
+import './PageHeader.css'
+
+const PageHeader = () => {
+  return (
+    <div className='page-header'>
+      <Container>
+        <nav className='main-navigation'>
+          <ul className='nav-list'>
+            <li className='nav-item'>
+              <NavLink to='/'>Home</NavLink>
+            </li>
+
+            <li className='nav-item'>
+              <NavLink to='/cities'>Cities</NavLink>
+            </li>
+
+            <li className='nav-item'>
+              <NavLink to='/counter'>Counter</NavLink>
+            </li>
+
+            <li className='nav-item'>
+              <NavLink to='/cars'>Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </Container>
+    </div>
+  )
+}
+
+export default PageHeader
