cecb77e4f5e56f277ac43a505542b165db0bcc58
RomanasV <romanasvenckus@gmail.com>
2023-10-18 13:24:29 +0300

    chuck norris api task done

--- src/pages/APIChuckNorrisPage/APIChickNorrisPage.js
+++ src/pages/APIChuckNorrisPage/APIChickNorrisPage.js
@@ -4,6 +4,7 @@ import Container from '../../Components/Container/Container'
 const APIChickNorrisPage = () => {
   const [joke, setJoke] = useState(null)
   const [categories, setCategories] = useState([])
+  const [searchInput, setSearchInput] = useState('')
 
   // useEffect(() => {
   //   fetch('https://api.chucknorris.io/jokes/random')
@@ -50,7 +51,38 @@ const APIChickNorrisPage = () => {
 
   const categoryFormHandler = (event) => {
     event.preventDefault()
-    console.log('veikia')
+
+    const selectedCategory = event.target.category.value
+    fetch(`https://api.chucknorris.io/jokes/random?category=${selectedCategory}`)
+      .then(res => res.json())
+      .then(joke => setJoke(joke.value))
+  }
+  
+  const searchFormHandler = (event) => {
+    event.preventDefault()
+
+    // const searchPhrase = event.target['search-input'].value
+    const searchPhrase = searchInput
+
+    fetch(`https://api.chucknorris.io/jokes/search?query=${searchPhrase}`)
+      .then(res => res.json())
+      .then(data => {
+        const jokes = data.result
+        const jokesLength = jokes.length
+
+        const randomIndex = Math.floor(Math.random() * jokesLength)
+        const randomJoke = jokes[randomIndex]
+
+        if (randomJoke) {
+          setJoke(randomJoke.value)
+        } else {
+          setJoke('No joke found :(')
+        }
+        
+        // event.target['search-input'].value = ''
+        // event.target.reset()
+        setSearchInput('')
+      })
   }
 
   const categoriesForm = categories.length > 0 ? (
@@ -66,12 +98,30 @@ const APIChickNorrisPage = () => {
     <p>Loading categories form...</p>
   )
 
+  const searchForm = categories.length > 0 ? (
+    <form onSubmit={searchFormHandler}>
+      <label htmlFor='search-input'>Search phrase:</label>
+      <input 
+        type='text' 
+        id='search-input' 
+        name='search-input' 
+        value={searchInput}
+        onChange={(event) => setSearchInput(event.target.value)}
+      />
+
+      <button type='submit'>Search for a joke</button>
+    </form>
+  ) : (
+    <p>Loading search form...</p>
+  )
+
   return (
     <Container>
       <h1>Chuck Norris API</h1>
       <p>{joke ? joke : 'Loading...'}</p>
       <button onClick={randomJokeHandler}>Get a Random joke</button>
       {categoriesForm}
+      {searchForm}
     </Container>
   )
 }
