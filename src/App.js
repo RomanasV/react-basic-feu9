834779a903fd455333f520c448292c461fa1e190
RomanasV <romanasvenckus@gmail.com>
2023-10-19 08:36:22 +0300

    api project task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import ContactUsPage from './pages/ContactUsPage'
 import NewsPage from './pages/NewsPage'
 import CounterPage from './pages/CounterPage'
@@ -27,6 +27,24 @@ function App() {
         <Route path='/api/chuck-norris' element={<APIChickNorrisPage />} />
         <Route path='/api/dogs' element={<DogsPage />} />
         <Route path='/api/ai' element={<AIPage />} />
+
+        <Route path='/api-project' element={
+          <div>
+            <h1>Home page:</h1>
+            <div>
+              <Link to='/api-project/posts'>Posts</Link>
+            </div>
+            <div>
+              <a href='/api-project/posts'>Posts</a>
+            </div>
+          </div>
+        } />
+        <Route path='/api-project/posts' element={<h1>Posts page:</h1>} />
+        <Route path='/api-project/albums' element={<h1>Albums page:</h1>} />
+        <Route path='/api-project/users' element={<h1>Users page:</h1>} />
+        <Route path='/api-project/posts/1' element={<h1>Single post page:</h1>} />
+        <Route path='/api-project/albums/1' element={<h1>Single album page:</h1>} />
+        <Route path='/api-project/users/1' element={<h1>Single user page:</h1>} />
 
         <Route path='*' element={<h1>404: Page not found</h1>} />
       </Routes>
